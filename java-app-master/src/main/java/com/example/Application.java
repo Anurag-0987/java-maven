822a6f2c7f4034126dde561118df53788a2a0ef7
Anurag Jha <anurag.jha@teamarcs.com>
2023-04-29 23:15:08 +0530

    first commit

--- java-app-master/src/main/java/com/example/Application.java
+++ java-app-master/src/main/java/com/example/Application.java
@@ -7,17 +7,17 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class App {
+public class Application {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @PostConstruct
     public void init()
     {
-        Logger log = LoggerFactory.getLogger(App.class);
+        Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
     }
 
